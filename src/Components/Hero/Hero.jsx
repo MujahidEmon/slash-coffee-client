ad22951dc4c3928247e9efb80457097b6ed0f5ee
MujahidEmon <mujahidulislam.official@gmail.com>
2025-01-26 16:14:00 +0600

    designed upto folloe section of homePage

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@
 const Hero = () => {
     return (
         <div className="bg-[url('https://i.ibb.co.com/chyDRQL/6.jpg')]  bg-cover bg-center w-screen   lg:h-[600px] max-w-screen h-[280px] font-raleway">
-            <div className="ml-10 gap-y-2 lg:gap-y-5 w-xs lg:w-2xl flex flex-col justify-center h-full">
+            <div className="lg:ml-16 ml-10 gap-y-2 lg:gap-y-5 w-xs lg:w-2xl flex flex-col justify-center h-full">
                 <p className="font-rancho text-white md:text-5xl text-3xl">Would you like a Cup of Delicious Coffee?</p>
                 <p className="text-xs lg:text-base  text-white">It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
                 <button className="btn bg-[#E3B577] w-fit shadow-none border-0 font-rancho text-white text-xl">Learn More</button>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import Banner from "../../Components/Banner/Banner";
 import Hero from "../../Components/Hero/Hero";
+import { FaCoffee } from "react-icons/fa";
 
 
 const Home = () => {
@@ -7,6 +9,34 @@ const Home = () => {
         <div className="">
             <Hero></Hero>
             <Banner></Banner>
+
+            {/* Products section */}
+            <div className="bg-[url('https://i.ibb.co.com/fNtQhMV/1.png')] bg-cover bg-center">
+                <div className="flex flex-col items-center justify-center gap-3 my-14">
+                    <p>--- Sip & Savor ---</p>
+                    <h1 className="font-rancho text-3xl stroke-black lg:text-6xl drop-shadow-2xl">Our Popular Products</h1>
+                    <Link className="flex justify-center"><button className="flex gap-1 items-center font-rancho btn shadow-none border-0 text-white text-xl bg-[#E3B577]">Add Coffee <FaCoffee></FaCoffee></button></Link>
+                </div>
+            </div>
+
+
+            {/* Follow Section */}
+            <div className="lg:max-w-7xl max-w-xs mx-auto">
+                <div className="flex flex-col items-center my-14 justify-center">
+                    <p>Follow Us Now</p>
+                    <h1 className="font-rancho text-6xl">Follow on Instagram</h1>
+                </div>
+                <div className="grid lg:grid-cols-4 lg:gap-16 gap-7 grid-cols-3">
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 9.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 10.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 11.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 12.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 13.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 14.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 15.png" alt="" />
+                    <img className="lg:w-3xs lg:h-[256px] h-[112px] w-28" src="/src/assets/images/cups/Rectangle 16.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
