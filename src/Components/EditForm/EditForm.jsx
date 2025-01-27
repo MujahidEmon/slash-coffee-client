88ac9a7fd3497970aa2cd3a7ef57046538a8bbb3
MujahidEmon <mujahidulislam.official@gmail.com>
2025-01-28 00:28:21 +0600

    update funtionality added

--- src/Components/EditForm/EditForm.jsx
+++ src/Components/EditForm/EditForm.jsx
@@ -1,8 +1,50 @@
+import Swal from "sweetalert2";
+
 const EditForm = ({coffee}) => {
   const {name, chef, category, photo, taste, supplier, _id, details} = coffee;
+
+  const handleUpdateCoffee = e => {
+      e.preventDefault();
+  
+      const form = new FormData(e.currentTarget);
+  
+      const name = form.get('name')
+      const chef = form.get('chef')
+      const supplier = form.get('supplier')
+      const taste = form.get('taste')
+      const category = form.get('category')
+      const details = form.get('details')
+      const photo = form.get('photo')
+  
+      const updatedCoffee = {name, chef, supplier, taste, category, details, photo}
+      console.log(updatedCoffee);
+  
+  
+      // Sending data to Server
+      fetch(`http://localhost:5000/coffees/${_id}`, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedCoffee)
+      })
+      .then(res=> res.json())
+      .then(data => {
+        console.log(data);
+        if(data.modifiedCount>0){
+          Swal.fire({
+            title: 'Coffee Details Updated Successfully',
+            // text: 'Do you want to continue',
+            icon: 'success',
+            confirmButtonText: 'Back'
+          })
+        }
+      })
+    }
+  
   return (
     <div>
-      <form>
+      <form onSubmit={handleUpdateCoffee}>
         <div className="grid sm:grid-cols-2 gap-6">
           <div>
             <label className="text-gray-600 text-sm mb-2 block font-bold">
